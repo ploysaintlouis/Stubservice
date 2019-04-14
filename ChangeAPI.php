c0ce8985c533cfc38da3bcdff60c4a7242024ff0
ploy.zenlouis@gmail.com <mp236ab123>
2019-04-15 01:41:39 +0700

    20190415

--- ChangeAPI.php
+++ ChangeAPI.php
@@ -8,6 +8,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 $functionNo = $data['changeRequestInfo[functionNo]'];	//functionname = FR01'
 $functionVersion = $data['changeRequestInfo[functionVersion]'];	//version V.1
 $projectId = $data['projectInfo'];
+$functionDescription = $data['FRHeader['.$functionNo.'][functionDesc]'];
 
 require_once('database.php');
 include('change_func.php');
@@ -16,7 +17,8 @@ include('random.php');
 	$strsql = "SELECT * FROM m_users  ";
 	$objExec = odbc_exec($objConnect, $strsql) or die ("Error Execute [".$strsql."]");
 	$USER = odbc_fetch_array($objExec);
-
+	//echo $USER['username'];
+	$username = $USER['username'];
 	//หา FR id
 	$strsql = searchFRId($functionNo,$functionVersion,$projectId);
 	$objExec = odbc_exec($objConnect, $strsql) or die ("Error Execute [".$strsql."]");	
@@ -26,58 +28,191 @@ include('random.php');
 	//หา จำนวนของ คำขอ change เพื่อรับค่า
 	$strsql = searchNumChange($functionId,$functionVersion);
 	$objExec = odbc_exec($objConnect, $strsql) or die ("Error Execute [".$strsql."]");	
-	$num_row = 0;
+	$num_row = 1;
 	$row = odbc_num_rows($objExec);
+	//$ResultNumChange = odbc_fetch_row($objExec);
+	//echo $ResultNumChange['CType'];
 	//echo $row;
-	//echo $ResultFR['ChangeType'];
-	while($num_row < $row)
+	//while($num_row <= $row)
+	/*
+	while(($ResultNumChange = odbc_fetch_array($objExec)) and ($num_row <= $row)){
+		echo $ResultNumChange['CType'];
+		echo $num_row ;
+		$num_row++;
+	}*/
+	if (null!=$row)
 	{
-		while($ResultNumChange = odbc_fetch_array($objExec)){
-			//echo "HH";
-			if($ResultNumChange['changeType'] == 'A'){
+		while(($ResultNumChange = odbc_fetch_array($objExec)) and ($num_row <= $row)){
+			//echo $ResultNumChange['CType'];
+			//echo $num_row ;
+			if($ResultNumChange['CType'] == 'A'){
+				//echo 'A';
 				$createUser = $ResultNumChange['createUser'];
-				$strsql = searchFRImpact($functionId,$functionVersion,$projectId);
-				$objExec = odbc_exec($objConnect, $strsql) or die ("Error Execute [".$strsql."]");	
-				$x = 1;
-				while($ResultFR = odbc_fetch_array($objExec)){
-					$typeData[$x]= $ResultFR['typeData'];
-					$dataId[$x]= $ResultFR['dataId'];
-					$dataName[$x]= $ResultFR['dataName'];
-					$schemaVersionId[$x]= $ResultFR['schemaVersionId'];
-					$refTableName[$x]= $ResultFR['refTableName'];
-					$refColumnName[$x]= $ResultFR['refColumnName'];
-					$dataType[$x]= $ResultFR['dataType'];
-					$dataLength[$x]= $ResultFR['dataLength'];
-					$decimalPoint[$x]= $ResultFR['decimalPoint'];
-					$constraintPrimaryKey[$x]= $ResultFR['constraintPrimaryKey'];
-					$constraintUnique[$x]= $ResultFR['constraintUnique'];
-					$constraintDefault[$x]= $ResultFR['constraintDefault'];
-					$constraintNull[$x]= $ResultFR['constraintNull'];
-					$constraintMinValue[$x]= $ResultFR['constraintMinValue'];
-					$constraintMaxValue[$x]= $ResultFR['constraintMaxValue'];
 
-					$functionNo = ;
-					$strsql = InsertNewFRImpact($ResultFR,$createUser,$functionNo);
+				$strsql1  = searchFRMAXFuncNo();
+				$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql."]");
+				$Max_FRNO  = odbc_fetch_array($objExec1);
+				//echo $Max_FRNO['Max_FRNO'];
+				//echo substr($Max_FRNO['Max_FRNO'],6,7)+1 ;
+				$New_FRNO = substr($Max_FRNO['Max_FRNO'],0,7).(substr($Max_FRNO['Max_FRNO'],7,7)+1);
+				$New_functionversion = '1';
 
-/*
-					echo $x;
-					echo $dataId[1];
-					echo $dataName[1];
-					echo $schemaVersionId[1];
-					echo $refTableName[1];
-					echo $refColumnName[1];
-					echo $dataType[1]."<br/>";
-*/
-					//echo $x ;
-					$x++;
+				//Insert New FR_HEADER
+				$strsql1 = InsertNewFR_HEADER($functionDescription,$New_functionversion,$New_FRNO,$projectId,$username);
+				$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+				odbc_free_result($objExec1);
+				//UPDATE วันที่ FR เก่า
+				$strsql1 = UpdateFR_HEADER($functionVersion,$functionNo,$projectId,$username);
+				$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+
+				$strsql1 = searchFR_NEW($New_FRNO,$functionVersion,$projectId);
+				$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+				$New_ResultFR = odbc_fetch_array($objExec1);
+				$New_functionId = $New_ResultFR['functionId'];
+
+				//Insert New FR_DETAIL
+				$strsql1 = InsertFR_DETAIL($functionNo,$functionVersion,$New_functionId,$New_functionversion,$New_FRNO,$projectId,$username);
+				$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+				odbc_free_result($objExec1);
+				//INSERT FR ใหม่ ด้วย filed ใหม่ที่ Add
+				$strsql1 = InsertNewFR_DETAIL($ResultNumChange,$New_functionId,$New_functionversion,$New_FRNO,$projectId,$username);
+				$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+				odbc_free_result($objExec1);
+				//UPDATE วันที่ FR เก่า
+				$strsql1 = UpdateFR_DETAIL($functionVersion,$functionNo,$projectId,$username);
+				$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+				odbc_free_result($objExec1);
+
+				$returnData['$functionVersion'] = $functionVersion;
+				$returnData['$functionNo'] = $functionNo;
+				$returnData['$dataName'] = $ResultNumChange['dataName'];
+				$returnData['$changeType'] = $ResultNumChange['changeType'];
+				$returnData['$tableName'] = $ResultNumChange['tableName'];
+				$returnData['$columnName'] = $ResultNumChange['columnName'];
+
+				//echo $ResultNumChange['typeData'];
+				if ($ResultNumChange['typeData'] == 1)
+				{
+					$returnData['$typeData'] = 'Input';
+				}else{
+					$returnData['$typeData'] = 'Output';
+				}
+				//echo $returnData['$typeData'];
+			
+			}else if($ResultNumChange['CType'] == 'B'){	//รายการ change =edit
+				//echo 'B';
+				//UPDATE FR เฉพาะ field
+				if (null !=$New_functionId){
+					$strsql1 = UpdateFRField_DETAIL($ResultNumChange,$New_functionversion,$New_FRNO,$projectId,$username);
+					$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+					odbc_free_result($objExec1);
+
+					//หา Impact จากการ edit กับ FR อื่นๆ
+					$strsql1 = searchFRImpact($ResultNumChange,$functionId,$functionVersion,$projectId);
+					$objExec1 = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql."]");	
+					$row_FR = odbc_num_rows($objExec1);
+
+					if ($row_FR != 0){
+						//UPDATE วันที่ FR DETAIL เก่า ที่impact จากการ edit นี้
+						$Result_FRDETAIL = odbc_fetch_array($objExec1);
+						$strsql1 = UpdateFRField_DETAIL($ResultNumChange,$Result_FRDETAIL['functionVersion'],$Result_FRDETAIL['functionNo'],$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);		
+
+						//UPDATE วันที่ FR HEADER เก่าที่impact จากการ edit นี้
+						$strsql1 = UpdateFR_HEADER($Result_FRDETAIL['functionVersion'],$Result_FRDETAIL['functionNo'],$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);
+					}
+
+				}
+				else{
+					if ($num_row == '1'){ // ทำเฉพาะรายการ EDIT รายการแรก รายการต่อไป update เฉพาะ fleid
+						$strsql1  = searchFRMAXFuncVer($functionNo);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql."]");
+						$Max_FRVer  = odbc_fetch_array($objExec1);
+						//echo $Max_FRNO['Max_FRNO'];
+						//echo substr($Max_FRNO['Max_FRNO'],6,7)+1 ;
+						$Max_FRVer = $Max_FRVer+1;
+						
+						//Insert New FR_HEADER
+						$strsql1 = InsertNewFR_HEADER($functionDescription,$Max_FRVer,$functionNo,$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);
+						//UPDATE วันที่ FR เก่า
+						$strsql1 = UpdateFR_HEADER($functionVersion,$functionNo,$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);
+
+						//Insert New FR_DETAIL
+						$strsql1 = InsertFR_DETAIL($functionNo,$functionVersion,$New_functionId,$Max_FRVer,$functionNo,$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);
+					}
+					//UPDATE วันที่ FR เก่า
+					$strsql1 = UpdateFRField_DETAIL($ResultNumChange,$Max_FRVer,$functionNo,$projectId,$username);
+					$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+					odbc_free_result($objExec1);
+				}
+			}else if($ResultNumChange['CType'] == 'C'){	//รายการ change = delete
+				//echo "C";
+				if (null !=$New_functionId){
+					$strsql1 = DeleteFRField_DETAIL($ResultNumChange,$New_functionversion,$New_FRNO,$projectId,$username);
+					$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+					odbc_free_result($objExec1);
+
+					//หา Impact จากการ delete กับ FR อื่นๆ
+					$strsql1 = searchFRImpact($ResultNumChange,$functionId,$functionVersion,$projectId);
+					$objExec1 = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql."]");	
+					$row_FR = odbc_num_rows($objExec1);
+					if ($row_FR != 0){
+						//UPDATE วันที่ FR DETAIL เก่า ที่impact จากการ delete นี้
+						$Delete_FRDETAIL = odbc_fetch_array($objExec1);
+
+						$strsql1  = searchFRMAXFuncNo();
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql."]");
+						$Max_FRNO  = odbc_fetch_array($objExec1);
+						//echo $Max_FRNO['Max_FRNO'];
+						//echo substr($Max_FRNO['Max_FRNO'],6,7)+1 ;
+						$New_FRNO = substr($Max_FRNO['Max_FRNO'],0,7).(substr($Max_FRNO['Max_FRNO'],7,7)+1);
+						$New_functionversion = '1';
+						$delete_FRVer = $Delete_FRDETAIL['functionVersion'];
+
+						//Insert New FR_HEADER
+						$strsql1 = InsertNewFR_HEADER($functionDescription,$New_functionversion,$New_FRNO,$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);
+						//UPDATE วันที่ FR เก่า
+						$strsql1 = UpdateFR_HEADER($delete_FRVer,$functionNo,$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);	
+
+						$strsql1 = searchFR_NEW($New_FRNO,$functionVersion,$projectId);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						$New_ResultFR = odbc_fetch_array($objExec1);
+						$New_functionId = $New_ResultFR['functionId'];
+		
+						//Insert New FR_DETAIL
+						$strsql1 = InsertFR_DETAIL($functionNo,$functionVersion,$New_functionId,$New_functionversion,$New_FRNO,$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);
+						//Delete FR DETAIL ด้วย filed ใหม่ที่ delete
+						$strsql1 = DeleteFRField_DETAIL($ResultNumChange,$New_functionId,$New_functionversion,$New_FRNO,$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);
+						//UPDATE วันที่ FR เก่า activeflag ='0'
+						$strsql1 = UpdateFR_DETAIL($functionVersion,$functionNo,$projectId,$username);
+						$objExec1  = odbc_exec($objConnect, $strsql1) or die ("Error Execute [".$strsql1."]");
+						odbc_free_result($objExec1);
+					}					
 				}
 			}
+			$num_row++;
 		}
-		//echo $num_row;
-		$num_row++;
 	}
+		//echo $num_row;
 
-	$returnData = 'HH';
+	//$returnData = 'HH';
 	response($returnData);
 
 //function response($order_id,$amount,$response_code,$response_desc,$retuneData){
@@ -109,10 +244,32 @@ function response($returnData){
 	
 	//echo $returnData['testCaseNo'];
 	//echo $returnData['ChangeType'];
-	//echo $returnData['testcaseVersion'];	
-	$response = $returnData;
+	//echo $returnData['testcaseVersion'];
+
+	$functionNo = $returnData['$functionNo'];
+	$Typedata = $returnData['$typeData'];
+	$dataName = $returnData['$dataName'];
+
+	$response = array(
+					"AffectedRequirement"=>array(
+						array(
+							"$functionNo"=>array(
+							"functionVersion"=>$returnData['$functionVersion'],
+							"$Typedata"=>array(
+								"$dataName"=>array(
+									"refTableName"=>$returnData['$tableName'],
+									"refColumnName"=>$returnData['$columnName'],
+									"changeType"=>$returnData['$changeType']
+									)
+								)
+							)
+						)
+					)
+				);
+
 	$json_response = json_encode($response);
-	echo "AffectedTestCase"."<br/>";
+	//echo "AffectedRequirement"."<br/>";
+	var_dump($json_response);
 	echo $json_response."<br/>";
 }
 ?>
--- change_func.php
+++ change_func.php
@@ -13,36 +13,175 @@ function searchFRId($functionNo,$functionVersion,$projectId) {
  } 
 
  function searchNumChange($functionId,$functionVersion) {
-	$strsql = "SELECT createUser,case changeType WHEN 'add' THEN 'A' WHEN 'edit' THEN 'B' WHEN 'delete' THEN 'C' END AS changeType
+	$strsql = "SELECT case changeType WHEN 'add' THEN 'A' WHEN 'edit' THEN 'B' WHEN 'delete' THEN 'C' END AS CType,*
 	   FROM T_TEMP_CHANGE_LIST
 		where functionVersion = '$functionVersion' 
 		and functionId = '$functionId'
 		and confirmflag = '1' 
-		order by changeType
+		order by CType
 			";
-		//	echo $strsql ;
+		//echo $strsql ;
 		return $strsql ;
  } 
 //หา DETAIL ของ FR ที่ IMPACT เฉพาะ FR นั้น
- function searchFRImpact($functionId,$functionVersion,$projectId) {
-	$strsql = "SELECT *
-	   FROM M_FN_REQ_DETAIL
-		where functionVersion = '$functionVersion' 
-		and functionId = '$functionId'
-		and activeflag = '1' 
-		and projectId = '$projectId' 
+ function searchFRImpact($ResultNumChange,$functionId,$functionVersion,$projectId) {
+	$strsql = "SELECT a.*,b.functionDescription
+	   FROM M_FN_REQ_DETAIL a,M_FN_REQ_HEADER b
+		where a.functionVersion != '$functionVersion' 
+		and a.functionId != '$functionId'
+		AND a.dataName = '$ResultNumChange[dataName]'
+		and a.activeflag = '1' 
+		and a.projectid = '$projectId' 
+		AND a.projectid = b.projectid
+		AND a.functionNo = b.functionNo 
+		AND a.functionId = b.functionId
+		AND a.functionVersion = b.functionVersion
 			";
-		//	echo $strsql ;
+		//echo $strsql ;
 		return $strsql ;
  } 
- 						$strsql = " select max(testCaseNo) AS Max_TCNO from M_TESTCASE_HEADER ";
-						$objExec = odbc_exec($objConnect, $strsql) or die ("Error Execute [".$strsql."]");
-						$Max_TCNO  = odbc_fetch_array($objExec);
- function InsertNewFRImpact($param,$createUser,$functionNo) {
+ function searchFRMAXFuncNo() {
+
+ 	$strsql = " SELECT max(functionNo) AS Max_FRNO 
+	 			FROM M_FN_REQ_HEADER ";
+
+	//echo $strsql;
+	return $strsql;
+} 
+function searchFRMAXFuncVer($functionNo) {
+
+	$strsql = " SELECT max(functionVersion) AS Max_FRVer
+				FROM M_FN_REQ_HEADER 
+				WHERE functionNo = '$functionNo' ";
+
+   //echo $strsql;
+   return $strsql;
+} 
+//update activeflag = '0' FR เก่า
+function UpdateFR_HEADER($functionVersion,$functionNo,$projectId,$username) {
+	$currentDateTime = date('Y-m-d H:i:s');
+
+	$strsql = "UPDATE M_FN_REQ_HEADER
+			set updateDate = '$currentDateTime',
+			updateUser = '$username',
+			activeFlag = '0'	
+			WHERE functionVersion = '$functionVersion' 
+			ANd functionNo = '$functionNo'
+			and activeflag = '1' 
+			and projectid = '$projectId' ";
+		//echo $strsql ;
+		return $strsql ;
+ }  						
+function InsertNewFR_HEADER($functionDescription,$New_functionversion,$New_FRNO,$projectId,$username) {
 	$currentDateTime = date('Y-m-d H:i:s');
 	$strsql = "INSERT INTO M_FN_REQ_HEADER (functionNo, functionDescription, projectId, createDate, createUser, updateDate, updateUser,functionversion,activeflag) 
-	VALUES ('$functionNo', '{$param->functionDescription}', {$param->projectId}, '$currentDateTime', '{$createUser}', '$currentDateTime', '{$createUser}','1','1')";
-	//	echo $strsql ;
+	VALUES ('$New_FRNO', '{$functionDescription}', {$projectId}, '$currentDateTime', '{$username}', '$currentDateTime', '{$username}','$New_functionversion','1')";
+	//echo $strsql ;
 	return $strsql ;
+} 
+//หา functionId -อง FR ใหม่
+function searchFR_NEW($New_FRNO,$functionVersion,$projectId) {
+	$strsql = "SELECT functionId
+	   FROM M_FN_REQ_HEADER 
+		where functionVersion = '$functionVersion' 
+		ANd functionNo = '$New_FRNO'
+		and activeflag = '1' 
+		and projectid = '$projectId' 
+			";
+		//echo $strsql ;
+		return $strsql ;
+ } 
+
+function InsertFR_DETAIL($functionNo,$functionVersion,$New_functionId,$New_functionversion,$New_FRNO,$projectId,$username) {
+	$currentDateTime = date('Y-m-d H:i:s');
+
+	$strsql = "INSERT INTO M_FN_REQ_DETAIL 
+	SELECT '$projectId','$New_functionId','$New_FRNO','$New_functionversion',b.typeData,b.dataName,
+	b.schemaVersionId,b.refTableName,b.refColumnName,b.dataType,b.dataLength,b.decimalPoint,b.constraintPrimaryKey,
+	b.constraintUnique,b.constraintDefault,b.constraintNull,b.constraintMinValue,b.constraintMaxValue,'$currentDateTime',
+	NULL,'1','$currentDateTime','$username','$currentDateTime','$username'
+	FROM M_FN_REQ_DETAIL b
+			WHERE b.functionVersion = '$functionVersion' 
+			AND b.functionNo = '$functionNo'
+			AND b.activeflag = '1' 
+			AND b.projectid = '$projectId' 
+	";
+	//echo $strsql;
+	return $strsql ;
+} 
+//update activeflag = '0' FR เก่า
+function UpdateFR_DETAIL($functionVersion,$functionNo,$projectId,$username) {
+	$currentDateTime = date('Y-m-d H:i:s');
+
+	$strsql = "UPDATE M_FN_REQ_DETAIL
+			set effectiveEndDate = '$currentDateTime',
+			updateDate = '$currentDateTime',
+			updateUser = '$username',
+			activeFlag = '0'	
+			WHERE functionVersion = '$functionVersion' 
+			ANd functionNo = '$functionNo'
+			and activeflag = '1' 
+			and projectid = '$projectId' ";
+		//echo $strsql ;
+		return $strsql ;
+ }  
+ function InsertNewFR_DETAIL($ResultNumChange,$New_functionId,$New_functionversion,$New_FRNO,$projectId,$username) {
+	$currentDateTime = date('Y-m-d H:i:s');
+
+	$strsql = "INSERT INTO M_FN_REQ_DETAIL 
+	values('$projectId','$New_functionId','$New_FRNO','$New_functionversion','$ResultNumChange[typeData]','$ResultNumChange[dataName]',
+	'$ResultNumChange[schemaVersionId]','$ResultNumChange[tableName]','$ResultNumChange[columnName]','$ResultNumChange[newDataType]',
+	'$ResultNumChange[newDataLength]',	'$ResultNumChange[newScaleLength]','N','$ResultNumChange[newUnique]','$ResultNumChange[newDefaultValue]',
+	'$ResultNumChange[newNotNull]',	'$ResultNumChange[newMinValue]','$ResultNumChange[newMaxValue]',
+	'$currentDateTime',	NULL,'1','$currentDateTime','$username','$currentDateTime','$username')
+	";
+	//echo $strsql;
+	return $strsql ;
+} 
+function UpdateFRField_DETAIL($ResultNumChange,$functionVersion,$functionNo,$projectId,$username) {
+	$currentDateTime = date('Y-m-d H:i:s');
+	$fieldName = '';
+	if($ResultNumChange['newDataType'] != null){
+		$fieldName = " dataType = '$ResultNumChange[newDataType]' ,";
+	}
+	if($ResultNumChange['newDataLength']!= null){
+		$fieldName .= " dataLength = '$ResultNumChange[newDataLength]' ,";
+	}
+	if($ResultNumChange['newScaleLength'] != null){
+		$fieldName .= " decimalPoint = '$ResultNumChange[newScaleLength]' ,";
+	}
+	if($ResultNumChange['newDefaultValue'] != null){
+		$fieldName .= " constrraintDefault = '$ResultNumChange[newDefaultValue]' ,";
+	}
+	if($ResultNumChange['newMinValue'] != null){
+		$fieldName .= " ConstraintMinValue = '$ResultNumChange[newMinValue]' ,";
+	}	
+	if($ResultNumChange['newMaxValue'] != null){
+		$fieldName .= " ConstraintMaxValue = '$ResultNumChange[newMaxValue]' ,";
+	}					
+	$condition = $fieldName;
+	$strsql = "UPDATE M_FN_REQ_DETAIL
+			set $condition 
+			constraintUnique = '$ResultNumChange[newUnique]',
+			constraintNull = '$ResultNumChange[newNotNull]'
+			WHERE functionVersion = '$functionVersion' 
+			AND functionNo = '$functionNo'
+			and activeflag = '1' 
+			AND dataName = '$ResultNumChange[dataName]'
+			and projectid = '$projectId' ";
+		//echo $strsql ;
+		return $strsql ;
+ }  
+ function DeleteFRField_DETAIL($ResultNumChange,$functionVersion,$functionNo,$projectId,$username) {
+	$currentDateTime = date('Y-m-d H:i:s');
+					
+	$strsql = "DELETE FROM M_FN_REQ_DETAIL
+			WHERE functionVersion = '$functionVersion' 
+			AND functionNo = '$functionNo'
+			and activeflag = '1' 
+			AND dataName = '$ResultNumChange[dataName]'
+			and projectid = '$projectId' ";
+		//echo $strsql ;
+		return $strsql ;
  } 
 ?>
